4da9ebbec611f03cf4b703e90ee2104f4385966a
{shiva} <girishiva93@gmail.com>
2021-06-25 11:31:50 +0545

    making responsive of service and pop up message

--- js/popup.js
+++ js/popup.js
@@ -2,4 +2,7 @@ window.onload = function() {
     document.getElementById('button').onclick = function() {
       document.getElementById('modalOverlay').style.display = 'none'
     };
-  };
+};
+setTimeout(function(){
+  $('#modalOverlay').fadeOut('slow');
+}, 5000);
